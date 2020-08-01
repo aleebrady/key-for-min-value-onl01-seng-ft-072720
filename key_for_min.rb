8d2fd0fa3e2a7050a6720240906c060c525d2720
Aaron Brady <aleebrady@gmail.com>
2020-08-01 17:49:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   if (name_hash.length == 0)
-    min_value_key = nil 
+    key = nil 
   else
-    min_value_key = name_hash.first[0]
+    min_value = name_hash.first[0]
     min_value = name_hash.first[1]
     name_hash.each { |key, value|
-    min_value_key = key if value <= min_value }
+    min_value = key if value <= min_value }
   end
   min_value_key
 end
